f7e22711220881791ceb08866fe80c170e1b2592
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-19 07:20:31 -0600

    Add function to add divs to sketch container

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //Create function that adds specific number of divs to sketch container with specific class
 
+const sketchContainer = document.querySelector("#sketch-container");
+
+const sketchCell = document.sketchContainer.createElement("div");
+
+// sketchContainer.createElement(sketchCell);
+
 //Create a function that calculates and updates the height and width of sketch cells based on number of divs
 
 //Create event listener for Black button and sketch cells that changes color of cells (divs) to black upon mouseover
