ba409166039ede4870d2a9ff747d8f0e5ad005c5
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-19 06:41:56 -0600

    Add comments for needed functions

---
+++ script.js
@@ -0,0 +1,13 @@
+//Create function that adds specific number of divs to sketch container with specific class
+
+//Create a function that calculates and updates the height and width of sketch cells based on number of divs
+
+//Create event listener for Black button and sketch cells that changes color of cells (divs) to black upon mouseover
+
+//Create event listener for Select-a-color button and  sketch cells that changes color of cells (divs) to color specified by user upon mouseover
+
+//Create event listener for Random color button and sketch cells that changes color of cells (divs) to a random color upon mouseover
+
+//Create event listener for reset button to erase the sketch container
+
+//Create event listener for Change grid size button that changes grid size based on user selection - slider?
