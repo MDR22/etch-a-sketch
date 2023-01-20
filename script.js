2c3b5490af3ea0b69109b095c266be900de1197d
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2023-01-19 18:03:18 -0600

    Add sketchCellSize function

--- script.js
+++ script.js
@@ -13,9 +13,24 @@ function gridSize(number) {
     for (let i = 0; i < numberSquared; i++) {
         addElement();
     };
+    return number;
 };
 
 //Create a function that calculates and updates the height and width of sketch cells based on number of divs
+
+function sketchCellSize(number) {
+    const sketchCells = document.querySelectorAll(".sketchCell");
+    const sketchCellWidth = (500 / number);
+    const sketchCellHeight = (500 / number);
+    sketchCells.forEach(cell => {
+        cell.style.width = `${sketchCellWidth}px`;
+        cell.style.height = `${sketchCellHeight}px`;
+        cell.style.margin = "-1px"
+        cell.style.border = "1px solid black";
+    });
+    // newDivGrid.style.width = sketchCellWidth + "px";
+    // newDivGrid.style.height = sketchCellHeight + "px";
+};
 
 //Create event listener for Black button and sketch cells that changes color of cells (divs) to black upon mouseover
 
